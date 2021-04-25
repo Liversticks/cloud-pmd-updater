7a7a04dee7f568f25bca002ec8afe25b9f7bd959
Liversticks <55716846+Liversticks@users.noreply.github.com>
2021-04-25 14:16:34 -0700

    Add Gates AF

---
+++ CloudPMD.Gates/UpdateGates.cs
@@ -0,0 +1,101 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Json;
+using CloudPMD.Shared;
+
+namespace CloudPMD.Gates
+{
+    public static class UpdateGates
+    {
+        public static HttpClient httpClient = new HttpClient();
+        
+        [FunctionName("UpdateGates")]
+        public static async Task RunAsync([TimerTrigger("0 0 5 * * *")]TimerInfo myTimer,
+            [CosmosDB(
+                databaseName: "Shared-Free",
+                collectionName: "V1-pmdboard",
+                ConnectionStringSetting = "CosmosDBConnection",
+                Id = "internal-Pokémon-Mystery-Dungeon-Gates",
+                PartitionKey = "internal-Pokémon-Mystery-Dungeon-Gates"
+            )] V1GameMetadata runInfo,
+            [CosmosDB(
+                databaseName: "Shared-Free",
+                collectionName: "V1-pmdboard",
+                ConnectionStringSetting = "CosmosDBConnection"
+            )] IAsyncCollector<V1Entry> entries, 
+            ILogger log)
+        {
+            log.LogInformation($"Gates Updater function started execution at: {DateTime.Now}");
+
+            //Category format: xxxxxxxx-Category Name
+            //Platform format: xxxxxxxx-Platform Name
+            //Language format: xxxxxxxx-ENG/JPN
+            foreach (var category in runInfo.Categories)
+            {
+                foreach (var platform in runInfo.Platforms)
+                {
+                    foreach (var language in runInfo.Languages)
+                    {
+                        var categoryInfo = category.Split('-');
+                        var platformInfo = platform.Split('-');
+                        var languageInfo = language.Split('-');
+
+                        string url = $"https://speedrun.com/api/v1/leaderboards/{runInfo.GameID}/category/{categoryInfo[0]}?var-{runInfo.PlatformID}={platformInfo[0]}&var-{runInfo.LanguageID}={languageInfo[0]}&top=1&embed=players";
+                        log.LogInformation(url);
+
+                        var response = await httpClient.GetAsync(url);
+                        var resStream = await response.Content.ReadAsStreamAsync();
+                        Response result = await JsonSerializer.DeserializeAsync<Response>(resStream);
+
+                        if (response.IsSuccessStatusCode)
+                        {
+                            if (result.ResponseBody.Players.PlayerList.Count > 0 && result.ResponseBody.RunList.Count > 0)
+                            {
+                                // Check whether the player is a guest or not
+                                string playerName;
+                                if (string.Equals(result.ResponseBody.Players.PlayerList[0].Role, "guest", StringComparison.InvariantCultureIgnoreCase))
+                                {
+                                    var jsonString = await response.Content.ReadAsStringAsync();
+                                    playerName = Utils.GetGuestUser(jsonString);
+                                }
+                                else
+                                {
+                                    playerName = !string.IsNullOrEmpty(result.ResponseBody.Players.PlayerList[0].Names.Name) ?
+                                    result.ResponseBody.Players.PlayerList[0].Names.Name :
+                                    result.ResponseBody.Players.PlayerList[0].Names.NameJP;
+                                }
+
+                                var runTime = result.ResponseBody.RunList[0].Run.Times.PrimaryTime;
+                                var runDate = result.ResponseBody.RunList[0].Run.RunDate;
+                                var srcID = result.ResponseBody.RunList[0].Run.Id;
+
+                                var row = new V1Entry
+                                {
+                                    id = $"run-{runInfo.GameID}-{categoryInfo[0]}-{platformInfo[0]}-{languageInfo[0]}",
+                                    Game = "Pokémon Mystery Dungeon: Gates to Infinity",
+                                    Category = categoryInfo[1],
+                                    Platform = platformInfo[1],
+                                    Language = languageInfo[1],
+                                    Version = string.Empty,
+                                    Runner = playerName,
+                                    RunDate = runDate,
+                                    RunTime = runTime,
+                                    SRCLink = srcID
+                                };
+                                await entries.AddAsync(row);
+                            }
+                        }
+                        else
+                        {
+                            log.LogError($"Request to {url} failed. Error code: {response.StatusCode}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
